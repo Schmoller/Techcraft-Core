fbef97c5a5d9fa236fa95dee3456f0e9fb6b39ab
Steven Schmoll <steven.schmoll@gmail.com>
2021-06-25 21:53:15 +1000

    Added all built-in shader sources.
    These are not currently compiled or linked in or anything.
    It is up to the using application to handle that.

---
+++ shaders/builtin/standard-vert.glsl
@@ -0,0 +1,30 @@
+#version 450
+#pragma shader_stage(vertex)
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(binding = 0) uniform CameraUBO {
+    mat4 view;
+    mat4 proj;
+} cam;
+
+layout(set = 1, binding = 1) uniform EntityUBO {
+    mat4 transform;
+    uint textureIndex;
+} obj;
+
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec3 inNormal;
+layout(location = 2) in vec4 inColor;
+layout(location = 3) in vec2 inTexCoord;
+
+layout(location = 0) out vec4 fragColour;
+layout(location = 1) out vec3 fragNormal;
+layout(location = 2) out vec3 fragTexCoord;
+
+
+void main() {
+    gl_Position = cam.proj * cam.view * obj.transform * vec4(inPosition, 1.0);
+    fragNormal = inNormal * mat3(obj.transform);
+    fragTexCoord = vec3(inTexCoord, obj.textureIndex);
+    fragColour = inColor;
+}
