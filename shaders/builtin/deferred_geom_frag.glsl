3e47b0ce171749685286b5054e26bcf54759b45c
Steven Schmoll <steven.schmoll@gmail.com>
2021-07-10 16:29:37 +1000

    Updated deferred_geom_frag.glsl to use normal map
    This makes use of the tangent vector to transform the tangent space normal vector into a world space normal vector.

--- shaders/builtin/deferred_geom_frag.glsl
+++ shaders/builtin/deferred_geom_frag.glsl
@@ -26,10 +26,21 @@ layout(location = 4) in vec4 fragPosition;
 layout(set = 2, binding = 3) uniform sampler2D albedo;
 layout(set = 3, binding = 4) uniform sampler2D normal;
 
+vec3 computeNormal() {
+    vec3 tangentNormal = texture(normal, fragTexCoord).xyz * 2.0 - 1.0;
+
+    vec3 worldNormal = normalize(fragNormal);
+    vec3 worldTangent = normalize(fragTangent);
+    vec3 worldBiTangent = normalize(cross(worldNormal, worldTangent));
+    mat3 tangentToWorldTransform = mat3(worldTangent, worldBiTangent, worldNormal);
+    return normalize(tangentToWorldTransform * tangentNormal);
+}
+
 void main() {
     vec4 color = texture(albedo, fragTexCoord) * fragColour;
+    vec3 normal = computeNormal();
 
     outPosition = fragPosition;
     outDiffuseOcclusion = vec4(color.rgb, 0);// TODO: Occlusion
-    outNormalRoughness = vec4(fragNormal, 0);// TODO: Roughness
+    outNormalRoughness = vec4(normal, 0);// TODO: Roughness
 }
